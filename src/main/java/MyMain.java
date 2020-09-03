f7ba8315107f618d267113490085c39bdf5108a0
camshaft54 <cam.shaw1234@gmail.com>
2020-09-03 12:40:37 -0700

    Complete Calculator Bot

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,21 +1,28 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MyMain {
 
     // Calculates the median of the three inputs
     public static int median(int a, int b, int c) {
-        return 0; // REPLACE WITH YOUR CODE
+        int[] nums = new int[] {a, b, c};
+        Arrays.sort(nums);
+        return nums[1];
     }
 
     // Returns the input with the larger absolute value
     public static int magnitude(int a, int b) {
-        return 0; // REPLACE WITH YOUR CODE
+        int mag = Math.max(Math.abs(a), Math.abs(b));
+        if (mag == Math.abs(a)) {
+            return a;
+        }
+        return b;
     }
 
     // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
     // where "a" and "b" are the inputs to the method
     public static double pythagoras(int a, int b) {
-        return 0.0; // REPLACE WITH YOUR CODE
+        return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
     }
 
     public static void main(String[] args) {
@@ -25,6 +32,35 @@ public class MyMain {
         System.out.println(pythagoras(3, 4)); // should be 5.0
 
         Scanner scan = new Scanner(System.in);
-        // YOUR CODE HERE
+        System.out.println("Welcome to Calculator Bot!\nWhich program would you like to run?\n(median, magnitude, pythagoras)");
+        switch(scan.next()) {
+            case "median":
+                int[] med_nums = new int[3];
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("Median: Enter int " + (i+1));
+                    med_nums[i] = scan.nextInt();
+                }
+                System.out.println("The median of " + Arrays.toString(med_nums) + " is " + median(med_nums[0], med_nums[1], med_nums[2]));
+                break;
+            case "magnitude":
+                int[] mag_nums = new int[2];
+                for (int i = 0; i < 2; i++) {
+                    System.out.println("Magnitude: Enter int " + (i+1));
+                    mag_nums[i] = scan.nextInt();
+                }
+                System.out.println("The number with the highest magnitude of " + Arrays.toString(mag_nums) + " is " + magnitude(mag_nums[0], mag_nums[1]));
+                break;
+            case "pythagoras":
+                int[] pyt_nums = new int[2];
+                for (int i = 0; i < 2; i++) {
+                    System.out.println("Pythagoras: Enter int " + (i+1));
+                    pyt_nums[i] = scan.nextInt();
+                }
+                System.out.println("The length of the hypotenuse given the values" + Arrays.toString(pyt_nums) + " is " + pythagoras(pyt_nums[0], pyt_nums[1]));
+                break;
+            default:
+                System.out.println("Invalid Argument");
+        }
+        scan.close();
     }
 }
